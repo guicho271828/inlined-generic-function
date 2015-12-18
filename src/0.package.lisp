919a096334192f572450d171708196387b2e8366
Masataro Asai <guicho2.71828@gmail.com>
2015-12-18 21:41:09 +0900

    def

--- src/0.package.lisp
+++ src/0.package.lisp
@@ -9,4 +9,33 @@
   (:nicknames :inlined-gf))
 (in-package :inlined-generic-function)
 
-;; blah blah blah.
+;; target: implementing an inlined gf, a subclass of standard-generic-function
+;; cf.
+;; http://metamodular.com/CLOS-MOP/chapter-6.html
+;; http://metamodular.com/CLOS-MOP/readers-for-generic-function-metaobjects.html
+
+#+nil
+(defgeneric name (a b c)
+  (:generic-function-class inlined-generic-function)
+  (:method-class inlined-method))
+
+
+
+(defclass inlined-generic-function (standard-generic-function)
+     ()
+  (:default-initargs :method-class (find-class 'inlined-method))
+  (:metaclass funcallable-standard-class))
+
+(defclass inlined-method (standard-method)
+     (;; lambda-list
+      (method-lambda-expression :initarg :method-lambda-expression
+                                :accessor method-lambda-expression)))
+
+(defmethod make-method-lambda :around ((gf inlined-generic-function)
+                                       (m inlined-method)
+                                       lambda-expression environment)
+  (format t "~&setting ~A to ~a" lambda-expression m)
+  (setf (method-lambda-expression m)
+        (call-next-method)))
+
+;; generic-function-methods
