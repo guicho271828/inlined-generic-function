61f45787a42467219eac74f10587263461914789
Masataro Asai <guicho2.71828@gmail.com>
2015-12-19 12:55:18 +0900

    changed the matching criteria: considers inline declaration

--- src/2.compiler.lisp
+++ src/2.compiler.lisp
@@ -4,28 +4,41 @@
   "This function should be partially evaluated to be a correct compiler function.
 Returns an inlined form which is equivalent to calling the generic function."
   (declare (ignorable gf-orig whole env))
-  (if (member :inline-generic-function *features*)
-      (match whole
-        ((list* 'apply _)
-         (simple-style-warning "Failed to inline ~a: APPLY form cannot be inlined." whole)
-         whole)
-        ((and (or (list* name args)
-                  (list* 'funcall (list (or 'function 'quote) name) args))
-              (guard _ (fboundp name)
-                     (fdefinition name) (and gf (type inlined-generic-function))))
-         (format t "~&Inlining a generic function ~a~&" name)
-         (compile-generic-function gf args env whole))
-        ((and (or (list* name args)
-                  (list* 'funcall (list (or 'function 'quote) name) args))
-              (guard _ (fboundp name)
-                     (type-of (fdefinition name)) type))
-         (simple-style-warning "Failed to inline ~a: Inline information not available. ~a is a ~a, not ~a."
-                               whole name type 'inlined-generic-function)
-         whole)
-        (_
-         (simple-style-warning "Failed to inline ~a: The form does not match any of our expected cases." whole)
+  (match* (whole (member :inline-generic-function *features*))
+    (((list* 'apply _) forced)
+     (when forced
+       (simple-style-warning "Failed to inline ~a: APPLY form cannot be inlined." whole))
+     whole)
+    (((and (or (list* name args)
+               (list* 'funcall (list (or 'function 'quote) name) args))
+           (guard _ (fboundp name)
+                  (fdefinition name)
+                  (and gf (type inlined-generic-function))
+                  (multiple-value-list
+                   (function-information name env))
+                  (list :function       ;The first indicates the type of function definition or binding
+                        nil             ;The second value is true if NAME is bound locally.
+                        (assoc 'inline inline)))) ;The third value is an alist describing the declarations
+      forced)
+     (if (or (eq inline 'inline)
+             (and forced (not (eq inline 'notinline))))
+         (progn
+           (format t "~&Inlining a generic function ~a~&" name)
+           (compile-generic-function gf args env whole))
          whole))
-      whole))
+    (((and (or (list* name args)
+               (list* 'funcall (list (or 'function 'quote) name) args))
+           (guard _ (fboundp name)
+                  (type-of (fdefinition name)) type))
+      forced)
+     (when forced
+       (simple-style-warning "Failed to inline ~a: Inline information not available. ~a is a ~a, not ~a."
+                             whole name type 'inlined-generic-function))
+     whole)
+    ((_ forced)
+     (when forced
+       (simple-style-warning "Failed to inline ~a: The form does not match any of our expected cases." whole))
+     whole)))
 
 (defun compile-generic-function (gf args env whole)
   (declare (ignorable gf args env))
